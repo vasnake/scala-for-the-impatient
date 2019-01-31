d1a29464b13a1d90bf8267778cdbf3f248831523
vasnake <vasnake@gmail.com>
2019-02-01 00:44:29 +0300

    chapter 2

---
+++ src/main/scala/Chapter02/ControlStructuresAndFunctions.scala
@@ -0,0 +1,19 @@
+package Chapter02
+
+object ControlStructuresAndFunctions {
+// topics:
+    // conditional expressions
+    // statement termination
+    // block expressions and assignments
+    // input and output
+    // loops
+    // advanced 'for' loops
+    // functions
+    // default and named arguments
+    // variable arguments
+    // procedures
+    // lazy values
+    // exceptions
+
+// expressions: almost all constructs have values
+}
