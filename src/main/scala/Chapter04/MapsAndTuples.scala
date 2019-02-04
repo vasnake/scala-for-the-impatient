54841e8569b51c5608a2ddcc96b56591291080ce
vasnake <vasnake@gmail.com>
2019-02-05 00:34:11 +0300

    WIP: chapter 4

---
+++ src/main/scala/Chapter04/MapsAndTuples.scala
@@ -0,0 +1,110 @@
+package Chapter04
+
+object MapsAndTuples {
+// topics:
+    // constructing a map
+    // accessing map values
+    // updating map values
+    // iterating over maps
+    // sorted maps
+    // interoperating with java
+    // tuples
+    // zipping
+
+// hash table by default, tree map, map in general
+// collection of key/value pairs / (k,v) tuples
+// creating, querying and traversing maps
+
+    // constructing a map
+    def constructingAMap = {
+        ???
+    }
+
+    // accessing map values
+    def accessingMapValues = {
+        ???
+    }
+
+    // updating map values
+    def updatingMapValues = {
+        ???
+    }
+
+    // iterating over maps
+    def iteratingOverMaps = {
+        ???
+    }
+
+    // sorted maps
+    def sortedMaps = {
+        ???
+    }
+
+    // interoperating with java
+    def interoperatingWithJava = {
+        ???
+    }
+
+    // tuples
+    def tuples = {
+        ???
+    }
+
+    // zipping
+    def zipping = {
+        ???
+    }
+}
+
+object MapsAndTuples_Exercises {
+
+    // map of prices; with discount
+    def ex1 = {
+        ???
+    }
+
+    // read words from a file; count words with mutable map
+    def ex2 = {
+        ???
+    }
+
+    // ex2 with immutable map
+    def ex3 = {
+        ???
+    }
+
+    // ex3 with a sorted map
+    def ex4 = {
+        ???
+    }
+
+    // ex4 with TreeMap
+    def ex5 = {
+        ???
+    }
+
+    // linked hash map for week days, insertion order
+    def ex6 = {
+        ???
+    }
+
+    // all java properties, aligned
+    def ex7 = {
+        ???
+    }
+
+    // return (min, max) tuple
+    def ex8 = {
+        ???
+    }
+
+    // return (lt, eq, gt) tuple
+    def ex9 = {
+        ???
+    }
+
+    // zip two words
+    def ex10 = {
+        ???
+    }
+}
