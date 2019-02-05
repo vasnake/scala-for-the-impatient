aa906c5ef80670f1593b864aafc60728eb858c8d
vasnake <vasnake@gmail.com>
2019-02-05 23:14:07 +0300

    WIP: chapter 5

---
+++ src/main/scala/Chapter05/Classes.scala
@@ -0,0 +1,109 @@
+package Chapter05
+
+object Classes {
+// topics:
+    // simple classes and parameterless methods
+    // properties with getters and setters
+    // properties with only getters
+    // object-private fields
+    // bean properties
+    // auxiliary constructors
+    // the primary constructor
+    // nested classes
+
+    // simple classes and parameterless methods
+    def simpleClassesAndParameterlessMethods = {
+        ???
+    }
+
+    // properties with getters and setters
+    def propertiesWithGettersAndSetters = {
+        ???
+    }
+
+    // properties with only getters
+    def propertiesWithOnlyGetters = {
+        ???
+    }
+
+    // object-private fields
+    def objectPrivateFields = {
+        ???
+    }
+
+    // bean properties
+    def beanProperties = {
+        ???
+    }
+
+    // auxiliary constructors
+    def auxiliaryConstructors = {
+        ???
+    }
+
+    // the primary constructor
+    def primaryConstructor = {
+        ???
+    }
+
+    // nested classes
+    def nestedClasses = {
+        ???
+    }
+
+}
+
+object Classes_Exercises {
+
+    // improve the Counter class: it doesn't turn negative at Int.MaxValue
+    def ex1 = {
+        ???
+    }
+
+    // write class BankAccount: methods deposit, withdraw, read-only property balance
+    def ex2 = {
+        ???
+    }
+
+    // write class Time: read-only properties hours, minutes, method before(other: Time): Boolean
+    def ex3 = {
+        ???
+    }
+
+    // rewrite class Time from ex3: internal repr. is the number of minutes since midnight
+    def ex4 = {
+        ???
+    }
+
+    // write class Student: read-write beans properties 'name', 'id'
+    // use javap to check the methods generated
+    def ex5 = {
+        ???
+    }
+
+    // class Person: provide a primary constructor that turns negative age to 0
+    def ex6 = {
+        ???
+    }
+
+    // write class Person: primary constructor("name lastname"); read-only properties firstName, lastName
+    def ex7 = {
+        ???
+    }
+
+    // write class Car: read-only properties for make, model, model year; read-write license plate
+    def ex8 = {
+        ???
+    }
+
+    // reimplement ex8 in java
+    def ex9 = {
+        ???
+    }
+
+    // class Employee: rewrite it to use explicit fields and default primary constructor
+    def ex10 = {
+        ???
+    }
+
+}
