d1a5c90b4ebdf450e3dccb8ea12027243a28be0a
vasnake <vasnake@gmail.com>
2019-02-07 00:14:05 +0300

    WIP: chapter 6

---
+++ src/main/scala/Chapter06/Objects.scala
@@ -0,0 +1,51 @@
+package Chapter06
+
+object Objects {
+// topics:
+    // singletons
+    // companion objects
+    // objects extending a class or trait
+    // the apply method
+    // application objects
+    // enumerations
+
+    // use object when you need a singleton, or home for values and functions
+    // or companion object
+
+    // singletons
+    def singletons = {
+        ???
+    }
+
+    // companion objects
+    def companionObjects = {
+        ???
+    }
+
+    // objects extending a class or trait
+    def objectsExtendingAClassOrTrait = {
+        ???
+    }
+
+    // the apply method
+    def theApplyMethod = {
+        ???
+    }
+
+    // application objects
+    def applicationObjects = {
+        ???
+    }
+
+    // enumerations
+    def enumerations = {
+        ???
+    }
+
+}
+
+object Objects_Exercises {
+    def ex1 = {
+        ???
+    }
+}
