6bd29e23fff39c95cf6e630a3811f80d81b0e0bd
vasnake <vasnake@gmail.com>
2019-02-07 15:57:02 +0300

    chapter 6 snippets

--- src/main/scala/Chapter06/Objects.scala
+++ src/main/scala/Chapter06/Objects.scala
@@ -19,32 +19,134 @@ object Objects {
 
     // singletons
     def singletons = {
-        ???
+        // scala has no static methods or fields
+        // use 'object' instead
+
+        // single instance, created on first use
+        // can't provide constructor/class parameters
+        object Accounts {
+            private var lastNumber = 0
+            def newUniqueNumber(): Int = { lastNumber += 1; lastNumber } // mutator
+        }
+        // constructor executes now:
+        println(s"next number: ${Accounts.newUniqueNumber()}")
     }
 
     // companion objects
     def companionObjects = {
-        ???
+        // class and objects share the same name;
+        // must be defined together, in one source file;
+        // can access each other private members
+
+        class Account {
+            // n.b. object's members are not in the scope
+            val id = Account.newUniqueNumber()
+            private var balance = 0.0
+            def deposit(amount: Double): Unit = { balance += amount } // mutator
+        }
+        object Account {
+            // only Account class can use this singleton
+            private var lastNumber = 0
+            private def newUniqueNumber(): Int = { lastNumber += 1; lastNumber } // mutator
+        }
     }
 
     // objects extending a class or trait
     def objectsExtendingAClassOrTrait = {
-        ???
+        // the result is an object of a class that extends ... + own features.
+        // e.g. useful application
+        abstract class UndoableAction(val description: String) {
+            def undo(): Unit
+            def redo(): Unit
+        }
+        // useful default action, need only one of them
+        object DoNothingAction extends UndoableAction("Do nothing") {
+            override def redo(): Unit = {}
+            override def undo(): Unit = {}
+        }
+        // usage
+        val actions = Map("open" -> DoNothingAction, "save" -> DoNothingAction)
     }
 
     // the apply method
     def theApplyMethod = {
-        ???
+        // 'apply' method as a companion class fabric method
+        val a = Array("Mary", "had") // object Array 'apply' method here
+
+        // confusion
+        val b = Array(1000) // one element of thousand
+        val c = new Array(1000) // 1000 elements of null
+
+        // e.g. you can use only fabric: private constructor
+        class Account private (val id: Int, initialBalance: Double) {
+            //val id = Account.newUniqueNumber()
+            private var balance = initialBalance
+            def deposit(amount: Double): Unit = { balance += amount }
+        }
+        object Account {
+            def apply(initialBalance: Double) = new Account(newUniqueNumber(), initialBalance)
+            private var lastNumber = 0
+            private def newUniqueNumber(): Int = { lastNumber += 1; lastNumber }
+        }
+        val acct = Account(1000.0)
     }
 
     // application objects
     def applicationObjects = {
-        ???
+        // 'main' method or App trait?
+
+        object HelloMain {
+            def main(args: Array[String]): Unit = {
+                args foreach println
+            }
+        }
+
+        object HelloApp extends App {
+            args foreach println
+        }
+        // scala -Dscala.time HelloApp // check it out
+        // App trait extends DelayedInit with compiler cpecial treatment
+
+        // n.b. compiler will be unhappy here:
+        // [warn]   Reason: companion contains its own main method, which means no static forwarder can be generated
+        // https://www.scala-sbt.org/release/docs/Howto-Logging.html
     }
 
     // enumerations
     def enumerations = {
-        ???
+        // scala does not have enumerated types, but
+
+        object TrafficLightColor extends Enumeration {
+            val Red, Yellow, Green = Value // three Value objects created with default (id, name)
+            // Value is an inner class
+            // id 0..maxint; name = field name
+            val Red2 = Value(0, "Stop")
+            val Yellow2 = Value(10) // name Yellow
+            val Green2 = Value("Go") // id 11
+        }
+
+        // n.b. the type of enumeration:
+        def doWhat(color: TrafficLightColor.Value) = {
+            color match {
+                case TrafficLightColor.Red => "stop"
+                case _ => "stop anyway"
+            }
+        }
+
+        // you can use alias
+        type Light = TrafficLightColor.Value
+        def doWhat2(color: Light) = {
+            import TrafficLightColor.{Red, Yellow, Green}
+            color match {
+                case Red => "stop"
+                case _ => "stop anyway"
+            }
+        }
+
+        // useful methods
+        TrafficLightColor.values.foreach(v => println(s"id: ${v.id}, name: ${v.toString}"))
+        // find by id or name
+        println(s"""by id: ${TrafficLightColor(0)}, by name: ${TrafficLightColor.withName("Red")}""")
     }
 
 }
