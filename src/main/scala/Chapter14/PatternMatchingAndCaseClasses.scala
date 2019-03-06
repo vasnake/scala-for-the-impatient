28951a67c1d89c6e37a891ffdd249b3d88166dda
vasnake <vasnake@gmail.com>
2019-03-06 14:24:12 +0300

    chapter 14 exercise 8

--- src/main/scala/Chapter14/PatternMatchingAndCaseClasses.scala
+++ src/main/scala/Chapter14/PatternMatchingAndCaseClasses.scala
@@ -683,19 +683,44 @@ object PatternMatchingAndCaseClasses_Exercises {
         assert(res == 18)
     }
 
-// 8. Extend the tree in the preceding exercise so that each nonleaf node stores an operator in
-// addition to the child nodes.
-// Then write a function 'eval' that computes the value.
-// For example, the tree
-//          +
-//        / |  \
-//       *  2   -
-//      /\      |
-//     3 8      5
-// has value (3 × 8) + 2 + (–5) = 21
-// Pay attention to the unary minus
+    // 8. Extend the tree in the preceding exercise so that each nonleaf node stores an operator in
+    // addition to the child nodes.
+    // Then write a function 'eval' that computes the value.
+    // For example, the tree
+    //          +
+    //        / |  \
+    //       *  2   -
+    //      /\      |
+    //     3 8      5
+    // has value (3 × 8) + 2 + (–5) = 21
+    // Pay attention to the unary minus
     def ex8 = {
-        ???
+        sealed abstract class BinaryTree
+        case class Leaf(value: Int) extends BinaryTree
+        case class Node(op: Char, children: BinaryTree*) extends BinaryTree
+
+        def eval(tree: BinaryTree): Int = tree match {
+            case Leaf(x) => x
+            case Node('+', xs @ _*) => xs.map(eval).sum
+            case Node('*', xs @ _*) => xs.map(eval).product
+            case Node('-', xs @ _*) => {
+                val lst = if (xs.length > 1) xs else Leaf(0) +: xs
+                lst.map(eval).reduceLeft(_ - _)
+            }
+            case Node(op, _) => throw new IllegalArgumentException(s"unknown operation: $op")
+        }
+
+        // test
+        val tree = Node('+',
+            Node('*', Leaf(3), Leaf(8)),
+            Leaf(2),
+            Node('-', Leaf(5)),
+            Node('-', Leaf(1), Leaf(9)))
+        // (3 × 8) + 2 + (0 – 5) + (1 - 9) = 13
+
+        val res = eval(tree)
+        println(s"eval: $res")
+        assert(res == 13)
     }
 
 // 9. Write a function that computes the sum of the non-None values in a List[Option[Int]]
