3e8e9b41abb7bad8f72b82708c1053f8e63ae65c
vasnake <vasnake@gmail.com>
2019-03-06 20:58:08 +0300

    WIP: chapter 15 snippets

--- build.sbt
+++ build.sbt
@@ -8,5 +8,11 @@ scalacOptions ++= Seq("-deprecation", "-feature")
 libraryDependencies ++= Seq(
     "io.reactivex" % "rxscala_2.12" % "0.26.5",
     "com.storm-enroute" %% "scalameter" % "0.10.1",
+    // annotations chapter
+    "org.hibernate.javax.persistence" % "hibernate-jpa-2.0-api" % "1.0.1.Final",
+    "javax.inject" % "javax.inject" % "1",
+    "org.checkerframework" % "checker-qual" % "2.7.0",
+    "com.novocode" % "junit-interface" % "0.11", // % Test
+    // tests
     "org.scalatest" %% "scalatest" % "3.0.5" % "test"
 )
--- src/main/scala/Chapter15/Annotations.scala
+++ src/main/scala/Chapter15/Annotations.scala
@@ -1,5 +1,12 @@
 package Chapter15
 
+import scala.beans._
+import javax.persistence._
+import javax.inject._
+import org.checkerframework.checker.i18n.qual._
+import org.junit._
+
+
 object Annotations {
 // topics:
     // what are annotations
@@ -10,16 +17,59 @@ object Annotations {
     // annotations for optimizations
     // annotations for errors and warnings
 
-    //
+    // information for compiler or external tools;
+    // in form: @annotation(...)
+    // java modifiers: @volatile, @transient, @strictfp, @native, @throws
+    // @tailrec
+    // assert with @elidable
+    // @deprecated
 
     // what are annotations
     def whatAreAnnotations = {
-        ???
+        // syntax is just like in java
+
+        // e.g. java frameworks annotations
+        @Test(timeout = 100) def testSomeFeature() = { ??? }
+        @Entity class Credentials {
+            @Id @BeanProperty var username: String = _
+            @BeanProperty var password: String = _
+        }
+
+        // scala annotations: processed by scala compiler (plugin)
+
+        // annotations can affect compiled code (in scala)
     }
 
     // what can be annotated
     def whatCanBeAnnotated = {
-        ???
+        // classes
+        // methods
+        // fields
+        // local variables
+        // parameters
+        // expressions
+        // type parameters
+        // types
+
+        @Entity class Credentials
+        @Test def testFunc() = {}
+        @BeanProperty var username = ""
+        def doStuff(@NotNull msg: String) = {}
+
+        // multiple annotations, order doesn't matter
+        @BeanProperty @Id var usernamE = ""
+
+        // annotating primary constructor, place annotation before and add parenthesis
+        class CredentialS @Inject() (var username: String)
+
+        // with expressions, after a colon
+        (Map(1->2).get(3): @unchecked) match { case a => println(a) }
+
+        // type parameters
+        class Container[@specialized T] { ??? }
+
+        // type (after type: method returns a localized string)
+        def country: String @Localized = ???
     }
 
     // annotations arguments
@@ -51,69 +101,71 @@ object Annotations {
 
 object Annotations_Exercises {
 
-    // 1. Write four JUnit test cases that use the @Test annotation with and without each of its
-    // arguments. Run the tests with JUnit.
+    // 1. Write four JUnit test cases that use the @Test annotation
+    // with and without each of its arguments.
+    // Run the tests with JUnit.
     def ex1 = {
         ???
     }
 
-    // 2. Make an example class that shows every possible position of an annotation. Use
-    //@deprecated as your sample annotation.
+    // 2. Make an example class that shows every possible position of an annotation.
+    // Use @deprecated as your sample annotation.
     def ex2 = {
         ???
     }
 
-    // 3. Which annotations from the Scala library use one of the meta-annotations @param, @field,
-    //@getter, @setter, @beanGetter, or @beanSetter?
+    // 3. Which annotations from the Scala library use one of the meta-annotations
+    // @param, @field, @getter, @setter, @beanGetter, or @beanSetter?
     def ex3 = {
         ???
     }
 
-    // 4. Write a Scala method sum with variable integer arguments that returns the sum of its
-    //arguments. Call it from Java.
+    // 4. Write a Scala method 'sum' with variable integer arguments that returns the sum of its
+    // arguments. Call it from Java.
     def ex4 = {
         ???
     }
 
-    // 5. Write a Scala method that returns a string containing all lines of a file. Call it from Java.
+    // 5. Write a Scala method that returns a string containing all lines of a file.
+    // Call it from Java.
     def ex5 = {
         ???
     }
 
-    // 6. Write a Scala object with a volatile Boolean field. Have one thread sleep for some time, then
-    //set the field to true, print a message, and exit. Another thread will keep checking whether the
-    //field is true. If so, it prints a message and exits. If not, it sleeps for a short time and tries
-    //again. What happens if the variable is not volatile?
+    // 6. Write a Scala object with a volatile Boolean field.
+    // Have one thread sleep for some time, then set the field to 'true', print a message, and exit.
+    // Another thread will keep checking whether the field is true.
+    // If so, it prints a message and exits. If not, it sleeps for a short time and tries again.
+    // What happens if the variable is not volatile?
     def ex6 = {
         ???
     }
 
     // 7. Give an example to show that the tail recursion optimization is not valid when a method can be
-    //overridden.
+    // overridden.
     def ex7 = {
         ???
     }
 
-    // 8. Add the allDifferent method to an object, compile and look at the bytecode. What
-    //methods did the @specialized annotation generate?
+    // 8. Add the 'allDifferent' method to an object, compile and look at the bytecode.
+    // What methods did the @specialized annotation generate?
     def ex8 = {
         ???
     }
 
-    // 9. The Range.foreach method is annotated as @specialized(Unit). Why? Look at the
-    //bytecode by running
-    //Click here to view code image
-    //javap -classpath /path/to/scala/lib/scala-library.jar
-    //scala.collection.immutable.Range
-    //and consider the @specialized annotations on Function1. Click on the
-    //Function1.scala link in Scaladoc to see them.
+    // 9. The Range.foreach method is annotated as @specialized(Unit).
+    // Why? Look at the bytecode by running
+    //  javap -classpath /path/to/scala/lib/scala-library.jar scala.collection.immutable.Range
+    // and consider the @specialized annotations on Function1.
+    // Click on the Function1.scala link in Scaladoc to see them.
     def ex9 = {
         ???
     }
 
-    // 10. Add assert(n >= 0) to a factorial method. Compile with assertions enabled and
-    //verify that factorial(-1) throws an exception. Compile without assertions. What
-    //happens? Use javap to check what happened to the assertion call.
+    // 10. Add assert(n >= 0) to a factorial method.
+    // Compile with assertions enabled and verify that factorial(-1) throws an exception.
+    // Compile without assertions. What happens?
+    // Use javap to check what happened to the assertion call.
     def ex10 = {
         ???
     }
