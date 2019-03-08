c7b3442b36bb2b303c99a9b2f0247a812cb5b5ba
vasnake <vasnake@gmail.com>
2019-03-08 15:25:42 +0300

    WIP: chapter 16

--- src/main/scala/Chapter16/XMLProcessing.scala
+++ src/main/scala/Chapter16/XMLProcessing.scala
@@ -92,80 +92,81 @@ object XMLProcessing {
 
 object XMLProcessing_Exercises {
 
-    // 1. What is <fred/>(0)? <fred/>(0)(0)? Why?
+    // 1. What is
+    // <fred/>(0)
+    // <fred/>(0)(0)
+    // Why?
     def ex1 = {
         ???
     }
 
     // 2. What is the result of
-    //Click here to view code image
-    //<ul>
-    //<li>Opening
-    // bracket: [</li>
-    //<li>Closing
-    // bracket: ]</li>
-    //<li>Opening
-    // brace: {</li>
-    //<li>Closing
-    // brace: }</li>
-    //</ul>
+    //  <ul>
+    //      <li>Opening bracket: [</li>
+    //      <li>Closing bracket: ]</li>
+    //      <li>Opening brace: {</li>
+    //      <li>Closing brace: }</li>
+    //  </ul>
     //How do you fix it?
     def ex2 = {
         ???
     }
 
     // 3. Contrast
-    //Click here to view code image
-    //<li>Fred</li> match { case <li>{Text(t)}</li> => t }
+    //  <li>Fred</li> match { case <li>{Text(t)}</li> => t }
     //and
-    //Click here to view code image
-    //<li>{"Fred"}</li> match { case <li>{Text(t)}</li> => t }
-    //Why do they act differently?
+    //  <li>{"Fred"}</li> match { case <li>{Text(t)}</li> => t }
+    // Why do they act differently?
     def ex3 = {
         ???
     }
 
-    // 4. Read an XHTML file and print all img elements that don’t have an alt attribute.
+    // 4. Read an XHTML file and print all 'img' elements that don’t have an 'alt' attribute.
     def ex4 = {
         ???
     }
 
-    // 5. Print the names of all images in an XHTML file. That is, print all src attribute values inside
-    //img elements.
+    // 5. Print the names of all images in an XHTML file.
+    // That is, print all 'src' attribute values inside 'img' elements.
     def ex5 = {
         ???
     }
 
     // 6. Read an XHTML file and print a table of all hyperlinks in the file, together with their URLs.
-    //That is, print the child text and the href attribute of each a element.
+    // That is, print the 'child' text and the 'href' attribute of each a element.
     def ex6 = {
         ???
     }
 
-    // 7. Write a function that has a parameter of type Map[String, String] and returns a dl
-    //element with a dt for each key and dd for each value. For example,
-    //Click here to view code image
-    //Map("A" -> "1", "B" -> "2")
-    //should yield <dl><dt>A</dt><dd>1</dd><dt>B</dt><dd>2</dd></dl>.
+    // 7. Write a function that has a parameter of type Map[String, String] and returns a 'dl'
+    // element with a 'dt' for each key and 'dd' for each value. For example,
+    //  Map("A" -> "1", "B" -> "2")
+    // should yield
+    //  <dl>
+    //      <dt>A</dt> <dd>1</dd>
+    //      <dt>B</dt> <dd>2</dd>
+    //  </dl>
     def ex7 = {
         ???
     }
 
-    // 8. Write a function that takes a dl element and turns it into a Map[String, String]. This
-    //function should be the inverse of the function in the preceding exercise, provided all dt
-    //children are distinct.
+    // 8. Write a function that takes a 'dl' element and turns it into a Map[String, String].
+    // This function should be the inverse of the function in the preceding exercise,
+    // provided all 'dt' children are distinct.
     def ex8 = {
         ???
     }
 
-    // 9. Transform an XHTML document by adding an alt="TODO" attribute to all img elements
-    //without an alt attribute, preserving everything else.
+    // 9. Transform an XHTML document by adding an
+    // alt="TODO"
+    // attribute to all 'img' elements without an alt attribute, preserving everything else.
     def ex9 = {
         ???
     }
 
     // 10. Write a function that reads an XHTML document, carries out the transformation of the
-    //preceding exercise, and saves the result. Be sure to preserve the DTD and any CDATA sections.
+    // preceding exercise, and saves the result.
+    // Be sure to preserve the DTD and any CDATA sections.
     def ex10 = {
         ???
     }
