50b90aed6a4d3084912c0cf542fb7f33ba7e152a
vasnake <vasnake@gmail.com>
2019-03-20 13:06:08 +0300

    chapter 19 exercise 9

--- src/main/scala/Chapter19/AdvancedTypes.scala
+++ src/main/scala/Chapter19/AdvancedTypes.scala
@@ -824,25 +824,41 @@ object AdvancedTypes_Exercises {
     }
 
     // 9. Consider this class that models a physical dimension:
-    //Click here to view code image
-    //abstract class Dim[T](val value: Double, val name: String) {
-    //protected def create(v: Double): T
-    //def +(other: Dim[T]) = create(value + other.value)
-    //override def toString() = s"$value $name"
-    //}
-    //Here is a concrete subclass:
-    //Click here to view code image
-    //class Seconds(v: Double) extends Dim[Seconds](v, "s") {
-    //override def create(v: Double) = new Seconds(v)
-    //}
-    //But now a knucklehead could define
-    //Click here to view code image
-    //class Meters(v: Double) extends Dim[Seconds](v, "m") {
-    //override def create(v: Double) = new Seconds(v)
-    //}
-    //allowing meters and seconds to be added. Use a self type to prevent that.
+    //  abstract class Dim[T](val value: Double, val name: String) {
+    //      protected def create(v: Double): T
+    //      def +(other: Dim[T]) = create(value + other.value)
+    //      override def toString() = s"$value $name"
+    //  }
+    // Here is a concrete subclass:
+    //  class Seconds(v: Double) extends Dim[Seconds](v, "s") {
+    //      override def create(v: Double) = new Seconds(v)
+    //  }
+    // But now a knucklehead could define
+    //  class Meters(v: Double) extends Dim[Seconds](v, "m") {
+    //      override def create(v: Double) = new Seconds(v)
+    //  }
+    // allowing meters and seconds to be added.
+    // Use a self type to prevent that.
     def ex9 = {
-        ???
+
+        abstract class Dim[T](val value: Double, val name: String) {
+            // just add one line of code:
+            this: T =>
+
+            protected def create(v: Double): T
+            def +(other: Dim[T]) = create(value + other.value)
+            override def toString() = s"$value $name"
+        }
+
+        class Seconds(v: Double) extends Dim[Seconds](v, "s") {
+            override def create(v: Double) = new Seconds(v)
+        }
+
+        // and this become illegal:
+        //class Meters(v: Double) extends Dim[Seconds](v, "m") { // self-type Meters does not conform to Dim[Seconds]'s selftype Dim[Seconds] with Seconds
+        //    override def create(v: Double) = new Seconds(v)
+        //}
+
     }
 
     // 10. Self types can usually be replaced with traits that extend classes, but there can be situations
